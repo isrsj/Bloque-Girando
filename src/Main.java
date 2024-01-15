a1d59391ca7797cb93792355d182de6d30ddec97
israel <jacobisraelreyessalas@gmail.com>
2024-01-14 22:39:19 -0600

    se crea la ventana y panel

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import javax.swing.*;
+
+/**
+ * @author Israel Reyes
+ */
 public class Main {
     public static void main(String[] args) {
-
+        JFrame frame = new JFrame("Examen Caro");
+        Panel panel = new Panel ();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setVisible(true);
+        frame.add(panel);
     }
 }
